dc4f0c8cc7ac045749a0a582f3b1fc92645c2d5c
Ali <46873759+Ghoreish@users.noreply.github.com>
2023-07-23 09:22:54 +0330

    Update dancing_light.bat
    - added delay option.
    - text is corrected

--- dancing_light.bat
+++ dancing_light.bat
@@ -1,24 +1,47 @@
 @echo off
-echo "1 black and white flashing"
-echo "2 all colors flashing"
+echo 1) black and white flashing
+echo 2) all colors flashing
 set /p selected="type 1 or 2: "
+set /p delay="delay between color changes: "
 if %selected% equ 1 (goto blackwhite)else (goto allcolors)
 
 :blackwhite
 cls
-color 07
-color 70
+for /L %%i in (1,1,%delay%) do (
+    color 07
+)
+for /L %%i in (1,1,%delay%) do (
+    color 70
+)
 goto blackwhite
 
 :allcolors
 cls
-color 07
-color 70
-color 10
-color 20
-color 30
-color 40
-color 50
-color 60
-color 80
+for /L %%i in (1,1,%delay%) do (
+    color 07
+)
+for /L %%i in (1,1,%delay%) do (
+    color 70
+)
+for /L %%i in (1,1,%delay%) do (
+    color 10
+)
+for /L %%i in (1,1,%delay%) do (
+    color 20
+)
+for /L %%i in (1,1,%delay%) do (
+    color 30
+)
+for /L %%i in (1,1,%delay%) do (
+    color 40
+)
+for /L %%i in (1,1,%delay%) do (
+    color 50
+)
+for /L %%i in (1,1,%delay%) do (
+    color 60
+)
+for /L %%i in (1,1,%delay%) do (
+    color 80
+)
 goto allcolors
